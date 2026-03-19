3134f236cc773b21cf2bdf86498a7a5279a835b6
Dmitry Anderson <4nd3r5z0n@gmail.com>
2026-03-19 08:55:27 +0100

    codegen setup added

---
+++ pkg/api/doc.go
@@ -0,0 +1,4 @@
+// Package api provides
+// - common boilerplate
+// - code generated from OpenAPI doc
+package api
---
+++ pkg/api/gen.go
@@ -0,0 +1,5 @@
+package api
+
+//go:generate oapi-codegen -config ../../oapi-codegen-types.yml ./api.yml
+//go:generate oapi-codegen -config ../../oapi-codegen-serv.yml ./api.yml
+//go:generate oapi-codegen -config ../../oapi-codegen-client.yml ./api.yml
